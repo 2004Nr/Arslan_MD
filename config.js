cc0e85ba8eb8db852dfb8156cc08901452dd4926
2004Nr <navodofficial98@gmail.com>
2025-09-07 12:56:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ARSLAN-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT01heUljaktvaUFzRDdCZW5WazJtSGZtTVNtc3JWRzAyNlJCRk0ya2kyUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmtKYnU0U3Q0b0NjVzliRmd3ckpzQnZ0ZlFZYU85bFE5VWlqQmUzVGsxND0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDSmVMYmo4R2RyOTVOdUZTZmRrOHRRWE12NDNHVHl0dFBVVTNONFhsYld3PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJKZ1drcUdGZTFzOUJ2UE55VFZxNXpSckFNNTFTOG9reXdYMFhQNkFqRjBJPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjZEYWtwck5nUG9OUDArK2drUlVIdVdIV002RTAvbG9EUi9IUmY2N0h3MlU9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImYwN3JwelAxM2FaNElqTTBzVmxsTTJzbGZId0dmdmJKMW5NMTRMQk03U2c9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK0V6Nm41aDJUT1Bzcno4c21IblRkN3pHa0IvRS93aGpnU2NHQkU5YWNFUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZHBWZHNicFNTdGtoZWZiL0ZyVGM0WGJRRmdkWFl6OHFXSG1LUmY4RHJUST0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im1rMlJxNWVzUnVJOVhTbm5KaDQ0N0FxTVZXQ09GR2xmb1BKeFZhV21JdXlVT3NDYTRRa3p3Qm9iUS81bFd6dHBobjFpQTZicFd5dTE0RDgyMGtOVUF3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjAyLCJhZHZTZWNyZXRLZXkiOiJzejgyaGt2Q0RwQzliUVRIZ1NNaUtDb2RSNUx3a1FzOG1VQkl5WXduUVdBPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwicmVnaXN0ZXJlZCI6dHJ1ZSwicGFpcmluZ0NvZGUiOiJEN1Q2NVpHSiIsIm1lIjp7ImlkIjoiOTQ3MDcxNjIwOTY6NTVAcy53aGF0c2FwcC5uZXQiLCJsaWQiOiIxMzI3MDY0MDI4Njk0Njg6NTVAbGlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNQR0h6OUVHRUlUbDlNVUdHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJuNm01WllhTUExQS9ZZmlWaEhLdUp4R2NmYUdHaDdkR1R0aTRmQzlpTUR3PSIsImFjY291bnRTaWduYXR1cmUiOiJlaWoxaGFIQW5XR3FBRXM4NWpHVmdRbnNMdFRpOWcyZGcvWHNhd0hybnd6U1Nla0tYSmFNSUdQRjh5cU83NllSdmtNL21ZRHZKY3VsYVV4TDhjY3NEZz09IiwiZGV2aWNlU2lnbmF0dXJlIjoiYXpGU2lvcmZXaTUrdW9Za2ppSktTSERnRmJrelBNa1BWZkxXUm90cHI2OEkyVU1mL3lKRjVsSElsYUZsK2tmWCswaWppT1VWeFB6cjZ5VTltSHJaQnc9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5NDcwNzE2MjA5Njo1NUBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJaK3B1V1dHakFOUVAySDRsWVJ5cmljUm5IMmhob2UzUms3WXVId3ZZakE4In19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwicm91dGluZ0luZm8iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDQUlJQ0E9PSJ9LCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3NTcyMjk3MDUsImxhc3RQcm9wSGFzaCI6IjNSOVozOSIsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBRXB0In0=",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -19,7 +19,7 @@ ANTI_DELETE: process.env.ANTI_DELETE || "true",
 // set true false for anti delete     
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
 // change it to 'same' if you want to resend deleted message in same chat     
-WELCOME: process.env.WELCOME || "true",
+WELCOME: process.env.WELCOME || "false",
 // true if want welcome and goodbye msg in groups    
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group
@@ -45,7 +45,7 @@ DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 OWNER_NUMBER: process.env.OWNER_NUMBER || "923237045919",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "ArslanMD Official",
+OWNER_NAME: process.env.OWNER_NAME || "Jonny Official",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*©𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 Arslan_MD*",
 // add bot owner name    
@@ -61,7 +61,7 @@ ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
-ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "true",
 // make anti link true,false for groups 
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
@@ -79,6 +79,6 @@ DEV: process.env.DEV || "923237045919",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
 // make it true for auto recoding 
 };
